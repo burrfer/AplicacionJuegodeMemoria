3f2ae8c420553d7f2eafeb509d1ea9ae1dccf195
user <user@DESKTOP-G5K0PCD>
2022-12-15 19:30:32 -0300

    Agregar archivos de proyecto.

---
+++ AplicacionJuegodeMemoria/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AplicacionJuegodeMemoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PanelJuego = new System.Windows.Forms.Panel();
+            this.btnReniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRecord = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PanelJuego
+            // 
+            this.PanelJuego.Location = new System.Drawing.Point(52, 31);
+            this.PanelJuego.Name = "PanelJuego";
+            this.PanelJuego.Size = new System.Drawing.Size(476, 483);
+            this.PanelJuego.TabIndex = 0;
+            // 
+            // btnReniciar
+            // 
+            this.btnReniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReniciar.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnReniciar.Location = new System.Drawing.Point(639, 411);
+            this.btnReniciar.Name = "btnReniciar";
+            this.btnReniciar.Size = new System.Drawing.Size(113, 62);
+            this.btnReniciar.TabIndex = 1;
+            this.btnReniciar.Text = "Reinicio del juego";
+            this.btnReniciar.UseVisualStyleBackColor = true;
+            this.btnReniciar.Click += new System.EventHandler(this.btnReniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(556, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Movimientos:";
+            // 
+            // lblRecord
+            // 
+            this.lblRecord.AutoEllipsis = true;
+            this.lblRecord.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblRecord.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblRecord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecord.Location = new System.Drawing.Point(696, 61);
+            this.lblRecord.Name = "lblRecord";
+            this.lblRecord.Size = new System.Drawing.Size(34, 27);
+            this.lblRecord.TabIndex = 3;
+            this.lblRecord.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 559);
+            this.Controls.Add(this.lblRecord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReniciar);
+            this.Controls.Add(this.PanelJuego);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelJuego;
+        private System.Windows.Forms.Button btnReniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRecord;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AplicacionJuegodeMemoria/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicacionJuegodeMemoria
+{
+    public partial class Form1 : Form
+    {   
+        //Configuramos la cantidad de filas y columnas del tablero
+        int TamanioColumnasFilas = 4;
+        int Movimientos = 0;
+        int CantidadDeCartasVolteadas = 0;
+        List<string> CartasEnumeradas;
+        List<string> CartasRevueltas;
+        ArrayList CartasSeleccionadas;
+        PictureBox CartaTemporal1;
+        PictureBox CartaTemporal2;
+        int CartaActual = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            iniciarJuego();
+        }
+
+        //Creamos un método que se llama iniciar juego.
+        //Con la finalidad de detener el reloj (timer) para que no siga avanzando el tiempo.
+        public void iniciarJuego() {
+            timer1.Enabled = false;
+            timer1.Stop();
+            lblRecord.Text = "0";
+            CantidadDeCartasVolteadas = 0;
+            Movimientos = 0;
+            PanelJuego.Controls.Clear();
+            CartasEnumeradas = new List<string>();
+            CartasRevueltas = new List<string>();
+            CartasSeleccionadas = new ArrayList();
+            for (int i = 0; i < 8; i++) {
+                //Utilizamos dos arreglos para insertar dos veces la cantidad de cartas, que son 8 con sus respectivos pares.
+                CartasEnumeradas.Add(i.ToString());
+                CartasEnumeradas.Add(i.ToString());
+            }
+            //Ahora generamos la posibilidad de que las cartas se mezclen de manera aleatoria (random).
+            //Para ello creamos la variable resultado para que se orden aleatoriamente las figuras.
+            var NumeroAleatorio = new Random();
+            var Resultado = CartasEnumeradas.OrderBy(item => NumeroAleatorio.Next());
+            foreach(string ValorCarta in Resultado) {
+                CartasRevueltas.Add(ValorCarta);
+            }
+            var tablaPanel = new TableLayoutPanel();
+            tablaPanel.RowCount = TamanioColumnasFilas;
+            tablaPanel.ColumnCount = TamanioColumnasFilas;
+            for (int i=0; i < TamanioColumnasFilas;i++)
+            {
+                var Porcentaje = 150f / (float)TamanioColumnasFilas - 10;
+                tablaPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, Porcentaje));
+                tablaPanel.RowStyles.Add(new RowStyle(SizeType.Percent, Porcentaje));
+
+            }
+            //Introducimos un contador de fichas.
+            int contadorFichas = 1;
+            for (var i = 0; i < TamanioColumnasFilas; i++)
+            {
+                for (var j = 0; j < TamanioColumnasFilas; j++)
+                {
+                    var CartasJuego = new PictureBox();
+                    CartasJuego.Name = string.Format("{0}", contadorFichas);
+                    CartasJuego.Dock = DockStyle.Fill;
+                    CartasJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+                    CartasJuego.Image = Properties.Resources.Girada;
+                    CartasJuego.Cursor = Cursors.Hand;
+                    CartasJuego.Click += btnCarta_Click; //Evento para validar toda acción de PictureBox
+                    tablaPanel.Controls.Add(CartasJuego, j, i);
+                    contadorFichas++;
+
+                }
+            }
+            tablaPanel.Dock = DockStyle.Fill;
+            PanelJuego.Controls.Add(tablaPanel);
+        }
+
+        private void btnReniciar_Click(object sender, EventArgs e)
+        {
+            iniciarJuego();
+        }
+
+        private void btnCarta_Click(object sender, EventArgs e)
+        {
+            if (CartasSeleccionadas.Count < 2){
+                Movimientos++;
+                lblRecord.Text = Convert.ToString(Movimientos); //NO OLVIDAR CONFIGURAR EL BOTÓN RECORD//
+                var CartasSeleccionadasUsuario = (PictureBox)sender;
+
+                CartaActual = Convert.ToInt32(CartasRevueltas[Convert.ToInt32(CartasSeleccionadasUsuario.Name) - 1]);
+                CartasSeleccionadasUsuario.Image = RecuperarImagen(CartaActual);
+                CartasSeleccionadas.Add(CartasSeleccionadasUsuario);
+                // 2 veces se realizó el evento del click.
+                    if (CartasSeleccionadas.Count == 2)
+                    {
+
+                        CartaTemporal1 = (PictureBox)CartasSeleccionadas[0];
+                        CartaTemporal2 = (PictureBox)CartasSeleccionadas[1];
+                        int Carta1 = Convert.ToInt32(CartasRevueltas[Convert.ToInt32(CartaTemporal1.Name) - 1]);
+                        int Carta2 = Convert.ToInt32(CartasRevueltas[Convert.ToInt32(CartaTemporal2.Name) - 1]);
+
+                    if (Carta1 != Carta2)
+                    {
+                        timer1.Enabled = true;
+                        timer1.Start();
+                    }
+
+                    else
+                    {
+                        CantidadDeCartasVolteadas++;
+                        if (CantidadDeCartasVolteadas > 7)
+                        {
+                            MessageBox.Show("El juego terminó");
+                        }
+                    CartaTemporal1.Enabled = false; 
+                    CartaTemporal2.Enabled = false;
+                    CartasSeleccionadas.Clear();
+                    
+                    }
+                }
+            }   
+            
+        }
+        public Bitmap RecuperarImagen(int NumeroImagen)
+        { 
+        Bitmap TmpImg = new Bitmap(200,100);
+        switch (NumeroImagen)
+        {
+            case 0: TmpImg = Properties.Resources.img11;
+               break;
+            default: TmpImg = (Bitmap)Properties.Resources.ResourceManager.GetObject("img"+NumeroImagen);
+               break;
+            }
+        return TmpImg;
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int TiempoVirarCarta = 1;
+            if (TiempoVirarCarta == 1)
+            {
+
+                CartaTemporal1.Image = Properties.Resources.Girada;
+                CartaTemporal2.Image = Properties.Resources.Girada;
+                CartasSeleccionadas.Clear();
+                TiempoVirarCarta = 0;
+                timer1.Stop();
+            }
+        }
+
+    }
+
+}
